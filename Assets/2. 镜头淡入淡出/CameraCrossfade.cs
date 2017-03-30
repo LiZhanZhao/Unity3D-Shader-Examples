252c717112017569cf0baad11d17c9939c306270
李展钊 <lzz0386@175game.com>
2017-03-30 17:10:11 +0800

    镜头淡入淡出效果

---
+++ Assets/2. 镜头淡入淡出/CameraCrossfade.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraCrossfade : MonoBehaviour {
+    public Shader shader = null;
+    public Color targetColor;
+    private Material _material;
+    protected Material material
+    {
+        get
+        {
+            if (_material == null)
+            {
+                if (shader == null)
+                {
+                    shader = Shader.Find("Camera/Crossfade");
+                }
+                _material = new Material(shader);
+                _material.hideFlags = HideFlags.DontSave;
+            }
+            return _material;
+        }
+    }
+
+    protected void OnEnable()
+    {
+        Camera camera = GetComponent<Camera>();
+        Debug.Assert(camera != null, "没有Camera组件");
+        camera.clearFlags = CameraClearFlags.SolidColor;
+        camera.backgroundColor = new Color(1, 1, 1, 0);
+        // 初始化
+        material.SetColor("_Color", targetColor);
+    }
+
+    
+   
+    
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        material.SetColor("_Color", targetColor);
+        if (material != null)
+        {
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+
+    }
+}
---
+++ Assets/2. 镜头淡入淡出/CameraCrossfade.shader
@@ -0,0 +1,53 @@
+﻿Shader "Camera/Crossfade" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "" {}
+		_Color ("Target Color", Color) = (0,0,0,1)
+		
+	}
+	
+	// Shader code pasted into all further CGPROGRAM blocks
+	CGINCLUDE
+	
+	#include "UnityCG.cginc"
+	
+	struct v2f {
+		float4 pos : SV_POSITION;
+		float2 uv : TEXCOORD0;
+	};
+	
+	sampler2D _MainTex;
+	fixed4 _Color;
+	sampler2D _bgTex;
+	
+	v2f vert( appdata_img v ) 
+	{
+		v2f o;
+		o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+		o.uv = v.texcoord.xy;
+		return o;
+	} 
+	
+	half4 frag(v2f i) : SV_Target 
+	{
+		float4 col = _Color * tex2D(_MainTex, i.uv);
+		return col;
+	}
+
+	ENDCG 
+	
+	Subshader {
+		Pass 
+		{
+			ZTest Always 
+			Cull Off 
+			ZWrite Off
+	  
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			ENDCG
+		 }
+	}
+
+	
+} // shader
