9a776592dc9d22425b10825b1fadea6fd5b612bd
李展钊 <lzz0386@175game.com>
2017-04-14 14:31:58 +0800

    模型Bloom效果

---
+++ Assets/10. 模型Bloom/Bloom.shader
@@ -0,0 +1,181 @@
+﻿Shader "Hidden/Bloom"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// pass 0 - blur the main texture
+		pass { 
+		    
+			ZTest Always Cull Off ZWrite Off Blend Off
+		
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			
+			#include "UnityCG.cginc"
+				
+			uniform sampler2D _MainTex;
+			uniform half4 _MainTex_TexelSize;
+			uniform half _BlurSpread;
+				
+			struct v2f {
+				half4 pos : SV_POSITION;
+				half2 uv : TEXCOORD0;
+				half2 uv2[4] : TEXCOORD1;
+			};	
+				
+			v2f vert (appdata_img v)
+			{
+				v2f o;
+				o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+		       	o.uv = v.texcoord;
+		       	
+		       	o.uv2[0] = v.texcoord + _MainTex_TexelSize.xy * half2(_BlurSpread, _BlurSpread);					
+				o.uv2[1] = v.texcoord + _MainTex_TexelSize.xy * half2(-_BlurSpread, -_BlurSpread);
+				o.uv2[2] = v.texcoord + _MainTex_TexelSize.xy * half2(_BlurSpread, -_BlurSpread);
+				o.uv2[3] = v.texcoord + _MainTex_TexelSize.xy * half2(-_BlurSpread, _BlurSpread);
+				return o;
+			}	
+			
+			fixed4 frag ( v2f i ) : COLOR
+			{
+				
+				fixed4 blur = tex2D(_MainTex, i.uv ) * 0.4;
+				blur += tex2D(_MainTex, i.uv2[0]) * 0.15;
+				blur += tex2D(_MainTex, i.uv2[1]) * 0.15;
+				blur += tex2D(_MainTex, i.uv2[2]) * 0.15;	
+				blur += tex2D(_MainTex, i.uv2[3]) * 0.15;
+				return blur;
+			}
+				 			
+			ENDCG	
+		}
+
+		// pass 1 - glow
+		Pass { 
+			name "Glow"
+			ZTest Always Cull Off ZWrite Off 
+			Blend Off
+			
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+	
+			#include "UnityCG.cginc"
+	
+			uniform sampler2D _MainTex;
+			uniform half4 _MainTex_TexelSize;
+			uniform float _GlowAlpha;
+			uniform float4 _GlowColorMultiplier;
+			uniform sampler2D _Blur;
+			
+			struct v2f {
+				half4 pos : SV_POSITION;
+				half2 uv : TEXCOORD0;
+				half2 uv1 : TEXCOORD1;
+			};	
+			
+			v2f vert (appdata_img v)
+			{
+				v2f o;
+				o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+		       	o.uv = v.texcoord;
+		       	o.uv1 = v.texcoord;
+		       	
+				#if UNITY_UV_STARTS_AT_TOP
+				if (_MainTex_TexelSize.y < 0) {
+					o.uv1.y = 1 - o.uv1.y;
+				}
+				#endif
+				
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : COLOR
+			{
+				// 方法1
+				/*
+				fixed4 mainTex = tex2D(_MainTex, i.uv);
+				half4 glow = tex2D(_Blur, i.uv1) * _GlowColorMultiplier;	
+				//根据alpha插值
+				// lerp(a,b f) = (1-f) * a + b * f  ==> f = 0 -> a, f = 1 -> b
+				fixed3 tempCol = lerp(mainTex.rgb, glow.rgb, _GlowAlpha) * glow.a;
+				fixed4 res = fixed4(tempCol, mainTex.a);
+				return res;
+				*/
+
+				
+				// 方法二
+				///*
+				fixed4 mainTex = tex2D(_MainTex, i.uv);
+				half4 glow = tex2D(_Blur, i.uv1);
+				fixed4 res;
+
+				// 经过pass2，这里就是把alpha为1的，变成mainTex
+				res.rgb = lerp(glow.rgb, mainTex.rgb, glow.a );
+				res.rgb = lerp(mainTex.rgb, res.rgb, glow.a * _GlowColorMultiplier.a);
+				res.a = mainTex.a;
+
+				return res;
+				//*/
+
+				
+			}
+			ENDCG 
+		}
+
+		// pass 2 - modify glow
+		Pass { 
+			name "Modify"
+			ZTest Always Cull Off ZWrite Off 
+			//Blend Off
+			
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+	
+			#include "UnityCG.cginc"
+	
+			uniform sampler2D _MainTex;
+			uniform float4 _GlowColorMultiplier;
+			
+			struct v2f {
+				half4 pos : SV_POSITION;
+				half2 uv : TEXCOORD0;
+				
+			};		
+			
+			v2f vert (appdata_img v)
+			{
+				v2f o;
+				o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+		       	o.uv = v.texcoord;
+		       	
+				
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : COLOR
+			{
+				// 方法二
+				
+				fixed4 mainTex = tex2D(_MainTex, i.uv);
+				fixed4 res;
+				// 颜色从外->内，慢慢加深
+				res.rgb =  _GlowColorMultiplier.rgb * mainTex.a;
+				// 主要就是把原来从外->内的alpha变大的，变成，从内->外的alpha变大
+				res.a = 1 - mainTex.a;
+				return res;
+
+				
+				
+				
+			}
+			ENDCG 
+		}
+	}
+}
---
+++ Assets/10. 模型Bloom/CameraBloom.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBloom : MonoBehaviour {
+
+    public Material bloomMaterial;
+    public int blurIterations = 4;
+    public float blurSpread = 1.0f;
+    public int blurTextureSize = 256;
+    public Color glowColorMultiplier = Color.white;
+    [Range(0,1)]
+    public float glowAlpha = 0;
+
+    private Camera shaderCamera;
+    private RenderTexture replaceRenderTexture;
+    void Start()
+    {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            Debug.Log("Image effects are not supported (do you have Unity Pro?)");
+            enabled = false;
+        }
+
+        
+        shaderCamera = new GameObject("Test", typeof(Camera)).GetComponent<Camera>();
+
+        Camera origCamera = GetComponent<Camera>();
+        replaceRenderTexture = new RenderTexture((int)origCamera.pixelWidth, (int)origCamera.pixelHeight, 0, RenderTextureFormat.ARGB32);
+        replaceRenderTexture.wrapMode = TextureWrapMode.Clamp;
+        replaceRenderTexture.useMipMap = false;
+        replaceRenderTexture.filterMode = FilterMode.Bilinear;
+        replaceRenderTexture.Create();
+
+        UpdateMatProperty();
+    }
+
+    void UpdateMatProperty()
+    {
+        if (bloomMaterial != null)
+        {
+            bloomMaterial.SetFloat("_BlurSpread", blurSpread);
+            bloomMaterial.SetColor("_GlowColorMultiplier", glowColorMultiplier);
+            bloomMaterial.SetFloat("_GlowAlpha", glowAlpha);       
+        }
+    }
+
+    public void OnPreRender()
+    {
+        Camera origCamera = GetComponent<Camera>();
+        shaderCamera.CopyFrom(origCamera);
+        //shaderCamera.backgroundColor = Color.clear;
+        shaderCamera.backgroundColor = new Color(0, 0, 0, 0);
+        shaderCamera.clearFlags = CameraClearFlags.SolidColor;
+        
+        shaderCamera.renderingPath = RenderingPath.Forward;
+        shaderCamera.targetTexture = replaceRenderTexture;
+
+        // 这里可以选择哪一些物体才进行发光,渲成replaceRenderTexture之后，再传入glowMaterial的"_Glow"
+        //shaderCamera.RenderWithShader(glowReplaceShader, "RenderType");
+
+    }
+
+	public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (bloomMaterial != null)
+        {
+
+            UpdateMatProperty();
+            RenderTexture blurA = RenderTexture.GetTemporary(blurTextureSize, blurTextureSize, 0, RenderTextureFormat.ARGB32);
+            RenderTexture blurB = RenderTexture.GetTemporary(blurTextureSize, blurTextureSize, 0, RenderTextureFormat.ARGB32);
+            RenderTexture blurRes = null;
+            // init blurB
+            Graphics.Blit(replaceRenderTexture, blurB);
+
+            for (int i = 0; i < blurIterations; ++i)
+            {
+                
+                if (i % 2 == 0)
+                {
+                    blurA.DiscardContents();
+                    // pass 0 - blur the main texture
+                    Graphics.Blit(blurB, blurA, bloomMaterial, 0);
+                    blurRes = blurA;
+                }
+                else
+                {
+                    blurB.DiscardContents();
+                    //blur
+                    Graphics.Blit(blurA, blurB, bloomMaterial, 0);
+                    blurRes = blurB;
+                }
+            }
+
+
+            // 测试模糊效果
+            //Graphics.Blit(blurRes, destination);
+
+            // 方法二
+            ///*
+            //bloomMaterial.SetTexture("_MainTex", blurRes);
+            RenderTexture blurC = RenderTexture.GetTemporary(blurTextureSize, blurTextureSize, 0, RenderTextureFormat.ARGB32);
+            Graphics.Blit(blurRes, blurC, bloomMaterial, 2);
+
+            // */ 
+            bloomMaterial.SetTexture("_Blur", blurC);
+             
+            Graphics.Blit(source, destination, bloomMaterial, 1);
+
+            blurRes = null;
+            RenderTexture.ReleaseTemporary(blurA);
+            RenderTexture.ReleaseTemporary(blurB);
+            RenderTexture.ReleaseTemporary(blurC);
+
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
