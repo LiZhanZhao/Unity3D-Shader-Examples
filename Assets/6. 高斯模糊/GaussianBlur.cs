966525ab6b58694f48a243f5e5a09af36ffb2a61
李展钊 <lzz0386@175game.com>
2017-04-14 11:36:02 +0800

    添加高斯模糊效果

---
+++ Assets/6. 高斯模糊/GaussianBlur.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class GaussianBlur : MonoBehaviour
+{
+
+    public Shader shader;
+    private Material material;
+
+    public float blurSize = 0.5f;
+    public int blurIterations = 2;
+
+    void Start()
+    {
+        //new mat
+        if (!material && shader != null)
+        {
+            material = new Material(shader);
+        }
+
+    }
+
+    void OnDisable()
+    {
+        if (material)
+        {
+            DestroyImmediate(material);
+        }
+
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material != null)
+        {
+           
+            int rtWidth = source.width / 2;
+            int rtHeight = source.height / 2;
+
+            // clone
+            RenderTexture tempSrc = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+            Graphics.Blit(source, tempSrc);
+            for (int i = 0; i < blurIterations; i++)
+            {
+                // 方案1
+                /*
+                for (int j = 0; j < 2; j++) 
+                {
+                    material.SetFloat("_BlurSize", blurSize + j);
+
+                    RenderTexture tempDes = RenderTexture.GetTemporary(rtWidth, rtHeight, 0, source.format);
+                    Graphics.Blit(tempSrc, tempDes, material, 0);
+                    RenderTexture.ReleaseTemporary(tempSrc);
+                    tempSrc = tempDes;
+
+
+                    tempDes = RenderTexture.GetTemporary(rtWidth, rtHeight, 0, source.format);
+                    Graphics.Blit(tempSrc, tempDes, material, 1);
+                    RenderTexture.ReleaseTemporary(tempSrc);
+                    tempSrc = tempDes;
+                }
+
+                rtWidth /= 2;
+                rtHeight /= 2;
+                 */
+
+                // 方案2
+                ///*
+                material.SetFloat("_BlurSize", blurSize);
+                RenderTexture tempDes = RenderTexture.GetTemporary(rtWidth, rtHeight, 0, source.format);
+                Graphics.Blit(tempSrc, tempDes, material, 0);
+                RenderTexture.ReleaseTemporary(tempSrc);
+                tempSrc = tempDes;
+
+
+                tempDes = RenderTexture.GetTemporary(rtWidth, rtHeight, 0, source.format);
+                Graphics.Blit(tempSrc, tempDes, material, 1);
+                RenderTexture.ReleaseTemporary(tempSrc);
+                tempSrc = tempDes;
+
+                rtWidth /= 2;
+                rtHeight /= 2;
+                // */ 
+            }
+            Graphics.Blit(tempSrc, destination);
+            RenderTexture.ReleaseTemporary(tempSrc);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+}
---
+++ Assets/6. 高斯模糊/GaussianBlur.shader
@@ -0,0 +1,108 @@
+﻿Shader "ImageEffect/Unlit/GaussianBlue" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+
+	CGINCLUDE
+		#include "UnityCG.cginc"
+		#pragma target 3.0
+
+		
+		static const half curve[7] = { 0.0205, 0.0855, 0.232, 0.324, 0.232, 0.0855, 0.0205 };
+
+		static const half4 curve4[7] = { half4(0.0205,0.0205,0.0205,0), 
+										 half4(0.0855,0.0855,0.0855,0), 
+										 half4(0.232,0.232,0.232,0),
+										 half4(0.324,0.324,0.324,1), 
+										 half4(0.232,0.232,0.232,0), 
+										 half4(0.0855,0.0855,0.0855,0), 
+										 half4(0.0205,0.0205,0.0205,0) };
+										 
+		
+		struct v2f_withBlurCoords8 
+		{
+			float4 pos : SV_POSITION;
+			half4 uv : TEXCOORD0;
+			half4 offs : TEXCOORD1;
+		};	
+
+		sampler2D _MainTex;
+		//  if it's a 1k x 1k texture, both x and y will be 1.0/1024.0
+		uniform half4 _MainTex_TexelSize;
+
+		uniform float _BlurSize;
+
+
+		v2f_withBlurCoords8 vertBlurHorizontal (appdata_img v)
+		{
+			v2f_withBlurCoords8 o;
+			o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+			
+			o.uv = half4(v.texcoord.xy,1,1);
+			// 水平方向的偏差值
+			o.offs = half4(_MainTex_TexelSize.xy * half2(1.0, 0.0) * _BlurSize,1,1);
+
+			return o; 
+		}
+		
+		v2f_withBlurCoords8 vertBlurVertical (appdata_img v)
+		{
+			v2f_withBlurCoords8 o;
+			o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+			
+			o.uv = half4(v.texcoord.xy,1,1);
+			o.offs = half4(_MainTex_TexelSize.xy * half2(0.0, 1.0) * _BlurSize,1,1);
+			 
+			return o; 
+		}	
+
+		half4 fragBlur8 ( v2f_withBlurCoords8 i ) : SV_Target
+		{
+			half2 uv = i.uv.xy; 
+			half2 netFilterWidth = i.offs.xy;  
+
+			// 这里从中心点偏移3个间隔，从最左边或者是最上边开始进行加权累加
+			half2 coords = uv - netFilterWidth * 3.0;  
+			
+			half4 color = 0;
+			// 加权平均
+  			for( int i = 0; i < 7; i++ )  
+  			{   
+				half4 tap = tex2D(_MainTex, coords);
+				// 像素值乘上对应的权值
+				color += tap * curve4[i];
+				// 移到下一个像素
+				coords += netFilterWidth;
+  			}
+			return color;
+		}
+
+	ENDCG
+
+
+	SubShader {	
+		ZTest Off Cull Off ZWrite Off Blend Off
+		Fog {Mode off}
+		
+		
+		
+		Pass 	//0 Blur Vertical
+		{ 	
+			CGPROGRAM			
+			#pragma vertex vertBlurVertical
+			#pragma fragment fragBlur8
+			//#pragma fragmentoption ARB_precision_hint_fastest 			
+			ENDCG		 
+		}
+		
+		Pass 	//1 Blur Horizontal
+		{ 	
+			CGPROGRAM			
+			#pragma vertex vertBlurHorizontal
+			#pragma fragment fragBlur8
+			//#pragma fragmentoption ARB_precision_hint_fastest 			
+			ENDCG		 
+		}
+	}
+	Fallback off
+}
