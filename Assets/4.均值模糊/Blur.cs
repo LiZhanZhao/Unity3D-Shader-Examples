09759d2fe22bded521fea495efc88fae200c2504
李展钊 <lzz0386@175game.com>
2017-04-14 11:33:12 +0800

    均值迷糊效果

---
+++ Assets/4.均值模糊/Blur.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Blur : MonoBehaviour {
+
+    public Shader shader;
+    private Material material;
+
+    public float blurSize = 0.5f;
+    public int blurIterations = 2;
+
+    void Start()
+    {
+        //new mat
+        if (!material && shader != null)
+        {
+            material = new Material(shader);
+        }
+            
+    }
+
+    void OnDisable()
+    {
+        if (material)
+        {
+            DestroyImmediate(material);
+        }
+            
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material != null)
+        {
+            material.SetFloat("_BlurSize", blurSize);
+            int rtWidth = source.width / 2;
+            int rtHeight = source.height / 2;
+
+            // clone
+            RenderTexture tempSrc = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+            Graphics.Blit(source, tempSrc);
+            for (int i = 0; i < blurIterations; i++)
+            {
+                RenderTexture tempDes = RenderTexture.GetTemporary(rtWidth, rtHeight, 0, source.format);
+                Graphics.Blit(tempSrc, tempDes, material);
+                RenderTexture.ReleaseTemporary(tempSrc);
+                tempSrc = tempDes;
+                rtWidth /= 2;
+                rtHeight /= 2;
+            }
+            Graphics.Blit(tempSrc, destination);
+            RenderTexture.ReleaseTemporary(tempSrc);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);  
+        }
+    }
+	
+}
---
+++ Assets/4.均值模糊/BlurImageEffect.shader
@@ -0,0 +1,59 @@
+﻿Shader "ImageEffect/Unlit/BlurBox" {
+	Properties {
+	_MainTex ("Base (RGB)", 2D) = "white" {}
+}
+
+SubShader {	
+	Pass {
+	  	ZTest Always  	ZWrite Off
+
+		CGPROGRAM
+		#pragma vertex vert
+		#pragma fragment frag
+					
+		#include "UnityCG.cginc"
+
+		struct appdata_t {
+			float4 vertex : POSITION;
+			float2 texcoord : TEXCOORD0;
+		};
+
+		struct v2f {
+			float4 vertex : SV_POSITION;
+			half2 texcoord : TEXCOORD0;
+			half2 taps[4] : TEXCOORD1; 
+		};
+
+		sampler2D _MainTex;
+		half4 _MainTex_TexelSize;
+		float _BlurSize;
+		
+		v2f vert (appdata_t v)
+		{
+			v2f o;
+			o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+			float2 blurOffsets = float2(1, 1) * _BlurSize * _MainTex_TexelSize.xy;
+			o.texcoord = v.texcoord;
+			// 像素的上下左右
+			o.taps[0] = o.texcoord + blurOffsets * half2(0,-1);
+			o.taps[1] = o.texcoord + blurOffsets * half2(0,1);
+			o.taps[2] = o.texcoord + blurOffsets * half2(-1,0);
+			o.taps[3] = o.texcoord + blurOffsets * half2(1,0);
+			return o;
+			
+			
+		}
+		
+		fixed4 frag (v2f i) : SV_Target
+		{
+			half4 color = tex2D(_MainTex, i.taps[0]);
+			color += tex2D(_MainTex, i.taps[1]);
+			color += tex2D(_MainTex, i.taps[2]);
+			color += tex2D(_MainTex, i.taps[3]); 
+			return color * 0.25;
+		}
+	ENDCG
+	}
+}
+Fallback off
+}
