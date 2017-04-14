52ab8f3881bace7e3691bddd16b496c4678ff47a
李展钊 <lzz0386@175game.com>
2017-04-14 11:43:35 +0800

    Bloom效果

---
+++ Assets/9. Glow(Bloom)/Glow.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glow : MonoBehaviour {
+
+    public Material glowMaterial;
+    public float blurSpread = 1.0f;
+    public float glowStrength = 1.2f;
+    public Color glowColorMultiplier = Color.white;
+
+    // renderTexture size
+    public int downsampleSize = 256;
+    // 模糊的循环次数
+    public int blurIterations = 4;
+
+
+    // private 
+    private Camera shaderCamera;
+    private RenderTexture replaceRenderTexture;
+
+	// Use this for initialization
+	void Start () {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            Debug.Log("Image effects are not supported (do you have Unity Pro?)");
+            enabled = false;
+        }
+        UpdateMatProperty();
+
+
+        shaderCamera = new GameObject("Test", typeof(Camera)).GetComponent<Camera>();
+
+        Camera origCamera = GetComponent<Camera>();
+        replaceRenderTexture = new RenderTexture((int)origCamera.pixelWidth, (int)origCamera.pixelHeight, 0, RenderTextureFormat.ARGB32);
+        replaceRenderTexture.wrapMode = TextureWrapMode.Clamp;
+        replaceRenderTexture.useMipMap = false;
+        replaceRenderTexture.filterMode = FilterMode.Bilinear;
+        replaceRenderTexture.Create();
+
+        glowMaterial.SetTexture("_Glow", replaceRenderTexture);
+
+	}
+
+
+    void UpdateMatProperty()
+    {
+        if (glowMaterial != null)
+        {
+            glowMaterial.SetFloat("_BlurSpread", blurSpread);
+            glowMaterial.SetFloat("_GlowStrength", glowStrength);
+            glowMaterial.SetColor("_GlowColorMultiplier", glowColorMultiplier);
+        }
+    }
+
+
+    public void OnPreRender()
+    {
+        Camera origCamera = GetComponent<Camera>();
+        shaderCamera.CopyFrom(origCamera);
+        shaderCamera.backgroundColor = Color.clear;
+        shaderCamera.clearFlags = CameraClearFlags.SolidColor;
+        shaderCamera.renderingPath = RenderingPath.Forward;
+        shaderCamera.targetTexture = replaceRenderTexture;
+
+        // 这里可以选择哪一些物体才进行发光,渲成replaceRenderTexture之后，再传入glowMaterial的"_Glow"
+        //shaderCamera.RenderWithShader(glowReplaceShader, "RenderType");
+
+    }
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (glowMaterial != null)
+        {
+            UpdateMatProperty();
+            RenderTexture blurA = RenderTexture.GetTemporary(downsampleSize, downsampleSize, 0, RenderTextureFormat.ARGB32);
+            RenderTexture blurB = RenderTexture.GetTemporary(downsampleSize, downsampleSize, 0, RenderTextureFormat.ARGB32);
+
+            // init blurB
+            Graphics.Blit(replaceRenderTexture, blurB, glowMaterial, 0);
+
+            //glowMaterial.SetTexture("_Glow", blurA);
+            //glowMaterial.SetTexture("_Glow", blurB);
+
+            for (int i = 0; i < blurIterations; ++i)
+            {
+                // 偶数
+                if (i % 2 == 0)
+                {
+                    blurA.DiscardContents();
+                    // pass 0 - blur the main texture
+                    Graphics.Blit(blurB, blurA, glowMaterial, 0);
+
+                }
+                // 奇数
+                else
+                {
+                    blurB.DiscardContents();
+                    // pass 0 - blur the main texture
+                    Graphics.Blit(blurA, blurB, glowMaterial, 0);
+                }
+
+            }
+
+            Graphics.Blit(source, destination, glowMaterial, 1);
+
+            RenderTexture.ReleaseTemporary(blurA);
+            RenderTexture.ReleaseTemporary(blurB);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+        
+    }
+}
---
+++ Assets/9. Glow(Bloom)/Glow.shader
@@ -0,0 +1,126 @@
+﻿Shader "Hidden/Glow"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		pass { // pass 0 - blur the main texture
+		    
+			ZTest Always Cull Off ZWrite Off
+
+			Fog { Mode Off }
+
+			Blend Off
+		
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			
+			#include "UnityCG.cginc"
+				
+			uniform sampler2D _MainTex;
+			uniform half4 _MainTex_TexelSize;
+			uniform half _BlurSpread;
+				
+			struct v2f {
+				half4 pos : SV_POSITION;
+				half2 uv : TEXCOORD0;
+				half2 uv2[4] : TEXCOORD1;
+			};	
+				
+			v2f vert (appdata_img v)
+			{
+				v2f o;
+				o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+		       	o.uv = v.texcoord;
+		       	
+		       	o.uv2[0] = v.texcoord + _MainTex_TexelSize.xy * half2(_BlurSpread, _BlurSpread);					
+				o.uv2[1] = v.texcoord + _MainTex_TexelSize.xy * half2(-_BlurSpread, -_BlurSpread);
+				o.uv2[2] = v.texcoord + _MainTex_TexelSize.xy * half2(_BlurSpread, -_BlurSpread);
+				o.uv2[3] = v.texcoord + _MainTex_TexelSize.xy * half2(-_BlurSpread, _BlurSpread);
+				return o;
+			}	
+			
+			fixed4 frag ( v2f i ) : COLOR
+			{
+				fixed4 blur = tex2D(_MainTex, i.uv ) * 0.4;
+				blur += tex2D(_MainTex, i.uv2[0]) * 0.15;
+				blur += tex2D(_MainTex, i.uv2[1]) * 0.15;
+				blur += tex2D(_MainTex, i.uv2[2]) * 0.15;	
+				blur += tex2D(_MainTex, i.uv2[3]) * 0.15;
+				return blur;
+			}
+				 			
+			ENDCG	
+		}
+
+
+		Pass { // pass 1 - glow
+			name "Glow"
+			ZTest Always Cull Off ZWrite Off
+			Fog { Mode Off }
+			Blend Off
+			
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+	
+			#include "UnityCG.cginc"
+	
+			uniform sampler2D _MainTex;
+			uniform half4 _MainTex_TexelSize;
+
+			//传入 blur 好的Texture
+			uniform sampler2D _Glow;
+			uniform half _GlowStrength;
+			uniform float4 _GlowColorMultiplier;
+			
+			struct v2f {
+				half4 pos : SV_POSITION;
+				half2 uv : TEXCOORD0;
+				half2 uv1 : TEXCOORD1;
+			};	
+			
+			v2f vert (appdata_img v)
+			{
+				v2f o;
+				o.pos = mul (UNITY_MATRIX_MVP, v.vertex);
+		       	o.uv = v.texcoord;
+		       	o.uv1 = v.texcoord;
+		       	
+				#if UNITY_UV_STARTS_AT_TOP
+				if (_MainTex_TexelSize.y < 0) {
+					o.uv1.y = 1 - o.uv1.y;
+				}
+				#endif
+				
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : COLOR
+			{
+				fixed4 mainTex = tex2D(_MainTex, i.uv);
+				half4 glow = tex2D(_Glow, i.uv1) * _GlowStrength * _GlowColorMultiplier;	
+				return mainTex + glow;
+
+				/*
+				#if GLOWEFFECT_BLEND_SCREEN
+				return 1 - ((1 - mainTex) * (1 - glow));
+				#elif GLOWEFFECT_BLEND_MULTIPLY		
+				return mainTex * glow;
+				#elif GLOWEFFECT_BLEND_SUBTRACT
+				return mainTex - glow;
+				#else // additive
+				return mainTex + glow;
+				#endif
+				*/
+			}
+			ENDCG 
+		}
+
+		
+	}
+}
